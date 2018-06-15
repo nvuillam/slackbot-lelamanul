bda671bf4aa2833baf1d0d8d9e1ccde174f32231
NicolasVuillamy <nicolas.vuillamy@gmail.com>
2018-06-16 01:37:57 +0200

    v0.3.7 Heroku troubleshooting

--- index.js
+++ index.js
@@ -119,5 +119,5 @@ controller.on('dialog_submission', function handler(bot, message) {
 http.createServer(function(request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end('Ok, dyno is awake.');
-}).listen(5000);
+}).listen(process.env.PORT || 5000); 
 
