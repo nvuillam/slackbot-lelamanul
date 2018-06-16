53a4f739e45ee70c0030fcd7363fd3a589572d20
NicolasVuillamy <nicolas.vuillamy@gmail.com>
2018-06-16 02:43:49 +0200

    v0.3.93 Troubleshooting

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ var dateFormat = require('dateformat')
 const Entities = require('html-entities').AllHtmlEntities;
 const htmlEntities = new Entities();
 var shuffleArray = require('shuffle-array')
-var stringSimilarity = require('string-similarity');
 
 // Promosify methods
 const setTimeoutPromise = util.promisify(setTimeout);
@@ -23,6 +22,11 @@ const dateFormatBot = function(dt) {
 /**
  * Configure & Create bot
  */
+if (!process.env.token) {
+    console.log('Error: Specify token in environment');
+    process.exit(1);
+}
+
 
 var config = {};
 if (!process.env.MONGOLAB_URI) {
@@ -33,24 +37,25 @@ if (!process.env.MONGOLAB_URI) {
 var mongoStorage = require('botkit-storage-mongo')({ mongoUri: process.env.MONGOLAB_URI })
 config = {
     storage: mongoStorage,
-    port: process.env.BOTKIT_PORT, 
+    port: process.env.PORT,
     interactive_replies: true,
     debug: true
 };
+console.log('Mongolab storage')
+
 
 var controller = Botkit.slackbot(config);
 
 controller.configureSlackApp( 
     { 
-        clientId: process.env.SLACKAPP_CLIENT_ID, 
-        clientSecret: process.env.SLACKAPP_CLIENT_SECRET,  
-        clientVerificationToken: process.env.SLACKAPP_CLIENT_VERIFICATION_TOKEN, 
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET, 
         scopes: ['bot','channels:write']
     } 
 ); 
 
 // Set up WebServer for hooks and oAuth
-controller.setupWebserver(process.env.BOTKIT_PORT,function(err,webserver) { 
+controller.setupWebserver(process.env.PORT,function(err,webserver) { 
     controller.createWebhookEndpoints(controller.webserver); 
 
     controller.createOauthEndpoints(controller.webserver,function(err,req,res) { 
@@ -65,7 +70,7 @@ controller.setupWebserver(process.env.BOTKIT_PORT,function(err,webserver) {
 // Start bot listener
 
 var bot = controller.spawn({
-    token: process.env.SLACKAPP_BOT_TOKEN 
+    token: process.env.token
 }).startRTM();
 
 // Handle events related to the websocket connection to Slack
@@ -95,7 +100,7 @@ eval(fs.readFileSync('./domains/jira/commands.js') + '')
 // Miscellaneous
 eval(fs.readFileSync('./domains/misc/commands.js') + '')
 
-// Interactive messages callbacks: redirect to global method built from callbackId
+// Interactive messages callbacks
 controller.on('interactive_message_callback', function(bot, message) {
     var callbackId = message.callback_id;
     var domain = callbackId.substring(0,callbackId.indexOf(':'))
@@ -105,20 +110,9 @@ controller.on('interactive_message_callback', function(bot, message) {
     global[functionName](bot,message) 
 });
 
-// Dialog submissions: redirect to global method built from callbackId
-controller.on('dialog_submission', function handler(bot, message) {
-    var callbackId = message.callback_id;
-    var domain = callbackId.substring(0,callbackId.indexOf(':'))
-    var method = callbackId.substring(callbackId.indexOf(':')+1)
-    var functionName = 'dialog_'+domain+'_'+method ;
-    console.log('DIALOG SUBMISSION: '+functionName+'\n'+JSON.stringify(message,null,2))
-    global[functionName](bot,message)    
-})
-
-
 // To keep Heroku's free dyno awake
 http.createServer(function(request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end('Ok, dyno is awake.');
-}).listen(5000); 
+}).listen(5000);
 
